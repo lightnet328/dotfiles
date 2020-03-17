9ab10ab2b9f47ddc02eec2499d0efaa4dfaee451
lightnet328 <lightnet328@gmail.com>
2020-03-18 03:37:52 +0900

    Update brew installer

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,5 +1,5 @@
 install_brew() {
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 }
 
 install_fomula() {
