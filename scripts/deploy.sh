27c63b0a326cfa711491c86b4be72b84a21b238f
lightnet328 <lightnet328@gmail.com>
2020-03-21 22:31:49 +0900

    Fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,18 @@
+get_files() {
+  for pathname in "$2"/*; do
+    if [ -d "$pathname" ]; then
+      get_files "$1" "$pathname"
+    else
+      echo "${pathname/$1}"
+    fi
+  done
+}
+
 deploy() {
-  local FILES="${DOTPATH}/files"
-  cd $FILES
-  for f in .??*
+  local FILES="$DOTPATH/files"
+  for f in $(get_files "$FILES" "$FILES")
   do
-    ln -snfv ${FILES}/${f} ${HOME}/${f}
+    mkdir -p $(dirname "$HOME$f")
+    ln -snfv "$FILES$f" "$HOME$f"
   done
 }
